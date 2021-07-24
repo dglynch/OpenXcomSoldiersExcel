4baf17aca4a92b1c4c51f0eb69dff26b4a49c6c4
Dan Lynch <dglynch@dglynch.com>
2021-07-24 13:55:55 -0400

    Use Apache POI to write names to Excel file

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,6 +8,8 @@ repositories {
 
 dependencies {
     implementation("org.yaml:snakeyaml:1.29")
+    implementation("org.apache.poi:poi:5.0.0")
+    implementation("org.apache.poi:poi-ooxml:5.0.0")
 
     testImplementation("org.junit.jupiter:junit-jupiter:5.7.1")
     testImplementation("org.hamcrest:hamcrest:2.2")
--- app/src/main/java/com/dglynch/xcom/ExcelFileWriter.java
+++ app/src/main/java/com/dglynch/xcom/ExcelFileWriter.java
@@ -1,9 +1,32 @@
 package com.dglynch.xcom;
 
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.IOException;
 import java.io.OutputStream;
 import java.util.List;
 
 public class ExcelFileWriter {
-    public static void writeSoldiers(List<Soldier> soldiers, OutputStream outputStream) {
+    public static void writeSoldiers(List<Soldier> soldiers, OutputStream outputStream) throws IOException {
+        XSSFWorkbook workbook = new XSSFWorkbook();
+        XSSFSheet sheet = workbook.createSheet("Sheet1");
+
+        Row headerRow = sheet.createRow(0);
+        headerRow.createCell(0).setCellValue("Id");
+        headerRow.createCell(1).setCellValue("Name");
+
+        int rowCount = 0;
+        for (Soldier soldier : soldiers) {
+            Row row = sheet.createRow(++rowCount);
+            row.createCell(0).setCellValue(rowCount);
+            row.createCell(1).setCellValue(soldier.getName());
+        }
+
+        for (int i = 0; i < 2; i++) {
+            sheet.autoSizeColumn(i);
+        }
+        workbook.write(outputStream);
     }
 }
